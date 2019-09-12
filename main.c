90694349e7cd1f16303abbebd0abe21fd3038ec2
Dhruvil Dave <dhruvil.dave@outlook.com>
2019-09-13 02:00:30 +0530

    Fri 13 Sep 2019 02:00:30 AM IST

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 // Mon 27 May 2019 02:07:07 PM IST
 // main.c
 
-#if !defined(__linux__)
-printf("Source code compliant only with Linux");
-exit(EXIT_FAILURE);
-
-#endif // __linux__
-
 #include "matrix.h"
 
 int main(void) {
+#if !defined(__linux__)
+    printf("Source code compliant only with Linux\n");
+    exit(EXIT_FAILURE);
+
+#endif // __linux__
+
     matrix *mat = mat_init();
     printf("Enter A:\n");
     mat_input(mat);
